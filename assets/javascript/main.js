6d8ceaef5ef9b430b2d2f59dc7b493cb02a890b2
Ezekiel Mangalu <emangalu@gmail.com>
2016-11-02 04:12:41 +0800

    Setup modal

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,3 +1,26 @@
+$(function() {
+  // Variables
+  $modal = $('#modal');
+  modalClose = $modal.find('.close');
+
+  // Bind events
+  // = Modal close event
+  modalClose.on('click', function(e) {
+    e.preventDefault();
+    modal.hide($modal);
+  });
+
+  // = Modal open event
+  $('.gallery-item').on('click', function(e) {
+    e.preventDefault();
+    $modal.find('img').attr('src', e.currentTarget.href);
+    $modal.find('.card').html($(this).find('.card').html());
+    modal.show($modal);
+  });
+
+});
+
+// -- Gallery
 var gallery = (function() {
   var data = [
     {
@@ -12,24 +35,43 @@ var gallery = (function() {
       link: "assets/images/image_3.jpg",
       width: "50"
     },
-		{
+    {
       title: "_04",
       text: "Lights by the water at night",
       link: "assets/images/image_4.jpg",
     },
-		{
+    {
       title: "_05",
       text: "Coconut trees on rocky breach",
       link: "assets/images/image_5.jpg"
     }
   ];
-   
+
   var $gallery = $('.gallery');
   var galleryItemTemplate = $("#gallery-item-template").html();
-  
+
   function _render(data) {
     $gallery.html(Mustache.render(galleryItemTemplate, {links: data}));
   }
-  
+
   _render(data);
+})();
+
+// -- Modal
+var modal = (function() {
+  function _show(elem) {
+    // console.log('showing..');
+    elem.show();
+  }
+
+  function _hide(elem) {
+    // console.log('hiding..');
+    elem.hide();
+  }
+
+  return {
+    show: _show,
+    hide: _hide,
+  };
+
 })();
